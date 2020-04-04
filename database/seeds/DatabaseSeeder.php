9b21742c529acb3e4bf0bcecd12db8e7e480df54
dmitryvasilievich81 <dmitryvasilievich81@gmail.com>
2020-04-04 18:39:23 +1000

    footer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(BanksTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(BanksTableSeeder::class);
+        // $this->call(ProductsTableSeeder::class);
+        // $this->call(CountriesTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
     }
 }
