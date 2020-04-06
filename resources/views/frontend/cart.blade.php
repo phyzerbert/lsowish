7934c5976a870d8b38ed0cab4b3c4b205de5026b
dmitryvasilievich81 <dmitryvasilievich81@gmail.com>
2020-04-07 01:26:00 +1000

    asdfa

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -21,85 +21,88 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12">
+                    @dump(is_Mobile())
                     <form>
                         <div class="cart-wraps">
-                            <div class="cart-table table-responsive" v-if="!is_mobile">
-                                <table class="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">{{__('page.product')}}</th>
-                                            <th scope="col">{{__('page.name')}}</th>
-                                            <th scope="col">{{__('page.price')}}</th>
-                                            <th scope="col">{{__('page.quantity')}}</th>
-                                            <th scope="col">{{__('page.total')}}</th>
-                                        </tr>
-                                    </thead>
+                            @if (!is_Mobile())
+                                <div class="cart-table table-responsive">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">{{__('page.product')}}</th>
+                                                <th scope="col">{{__('page.name')}}</th>
+                                                <th scope="col">{{__('page.price')}}</th>
+                                                <th scope="col">{{__('page.quantity')}}</th>
+                                                <th scope="col">{{__('page.total')}}</th>
+                                            </tr>
+                                        </thead>
 
-                                    <tbody>
-                                        <tr v-for="(item, index) of cart">
-                                            <td class="product-thumbnail">
-                                                <a href="javascript:;">
-                                                    <img :src="item.product.image" alt="Image">
-                                                </a>
-                                            </td>
+                                        <tbody>
+                                            <tr v-for="(item, index) of cart">
+                                                <td class="product-thumbnail">
+                                                    <a href="javascript:;">
+                                                        <img :src="item.product.image" alt="Image">
+                                                    </a>
+                                                </td>
 
-                                            <td class="product-name">
-                                                <a href="javascript:;">@{{item.product.name}}</a>
-                                            </td>
+                                                <td class="product-name">
+                                                    <a href="javascript:;">@{{item.product.name}}</a>
+                                                </td>
 
-                                            <td class="product-price">
-                                                <span class="unit-amount">RM @{{item.product.price}}</span>
-                                            </td>
+                                                <td class="product-price">
+                                                    <span class="unit-amount">RM @{{item.product.price}}</span>
+                                                </td>
 
-                                            <td class="product-quantity">
-                                                <div class="input-counter">
-                                                    <span class="minus-btn" @click="decrease(index)">
-                                                        <i class='bx bx-minus'></i>
-                                                    </span>
-                                                    <input type="text" v-model="item.quantity">
-                                                    <span class="plus-btn" @click="increase(index)">
-                                                        <i class='bx bx-plus'></i>
-                                                    </span>
-                                                </div>
-                                            </td>
+                                                <td class="product-quantity">
+                                                    <div class="input-counter">
+                                                        <span class="minus-btn" @click="decrease(index)">
+                                                            <i class='bx bx-minus'></i>
+                                                        </span>
+                                                        <input type="text" v-model="item.quantity">
+                                                        <span class="plus-btn" @click="increase(index)">
+                                                            <i class='bx bx-plus'></i>
+                                                        </span>
+                                                    </div>
+                                                </td>
 
-                                            <td class="product-subtotal">
-                                                <span class="subtotal-amount">@{{sub_total(item)}}</span>
+                                                <td class="product-subtotal">
+                                                    <span class="subtotal-amount">@{{sub_total(item)}}</span>
 
-                                                <a href="javascript:;" class="remove" @click="removeProduct(index)">
-                                                    <i class='bx bx-trash'></i>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-
-                            <div class="cart-mobile" v-else>
-                                <h3 class="text-center">{{__('page.product_list')}}</h3>
-                                <div class="row" v-for="(item, index) of cart">
-                                    <div class="col-12 mt-3">
-                                        <img :src="item.product.image" width="60" alt="Image">
-                                        <span class="product-name">@{{item.product.name}}</span>
-                                    </div>
-                                    <div class="col-12">
-                                        <span class="unit-amount">RM @{{item.product.price}}</span>
-                                        <div class="input-counter ml-3">
-                                            <span class="minus-btn" @click="decrease(index)">
-                                                <i class='bx bx-minus'></i>
-                                            </span>
-                                            <input type="text" v-model="item.quantity">
-                                            <span class="plus-btn" @click="increase(index)">
-                                                <i class='bx bx-plus'></i>
-                                            </span>
+                                                    <a href="javascript:;" class="remove" @click="removeProduct(index)">
+                                                        <i class='bx bx-trash'></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            @else
+                                <div class="cart-mobile">
+                                    <h3 class="text-center">{{__('page.product_list')}}</h3>
+                                    <div class="row" v-for="(item, index) of cart">
+                                        <div class="col-12 mt-3">
+                                            <img :src="item.product.image" width="60" alt="Image">
+                                            <span class="product-name">@{{item.product.name}}</span>
                                         </div>
-                                        <span class="subtotal-amount ml-4">@{{sub_total(item)}}</span>
-                                        <a href="javascript:;" class="remove float-right" style="margin-top: 5px" @click="removeProduct(index)">
-                                            <i class='bx bx-trash'></i>
-                                        </a>
+                                        <div class="col-12">
+                                            <span class="unit-amount">RM @{{item.product.price}}</span>
+                                            <div class="input-counter ml-3">
+                                                <span class="minus-btn" @click="decrease(index)">
+                                                    <i class='bx bx-minus'></i>
+                                                </span>
+                                                <input type="text" v-model="item.quantity">
+                                                <span class="plus-btn" @click="increase(index)">
+                                                    <i class='bx bx-plus'></i>
+                                                </span>
+                                            </div>
+                                            <span class="subtotal-amount ml-4">@{{sub_total(item)}}</span>
+                                            <a href="javascript:;" class="remove float-right" style="margin-top: 5px" @click="removeProduct(index)">
+                                                <i class='bx bx-trash'></i>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            @endif
 
                             <div class="cart-total mt-3 text-right font-weight-bold">
                                 Total <span class="ml-3" style="font-size: 20px"><b>RM <span>@{{get_total}}</span></b></span>
